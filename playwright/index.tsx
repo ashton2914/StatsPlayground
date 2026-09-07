8f983aa03450ffd00563daa55f5dbd7fd2f6bcce
ashton2914 <zhicheng2914@gmail.com>
2026-09-07 11:54:28 +0800

    test(analysis): align distribution coverage

--- playwright/index.tsx
+++ playwright/index.tsx
@@ -2,5 +2,4 @@ import "@fortawesome/fontawesome-free/css/all.min.css";
 import "../src/i18n";
 import "../src/index.css";
 import "../src/App.css";
-import "../src/components/distribution/distribution.css";
 import "../src/components/fitModel/fitModel.css";
